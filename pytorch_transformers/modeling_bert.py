8b783af5735e4d6b8d99bb87d75b67533c210f09
liangan1 <liangang.zhang@intel.com>
2019-12-04 10:18:56 +0800

    change qconfig to per_channel

--- pytorch_transformers/modeling_bert.py
+++ pytorch_transformers/modeling_bert.py
@@ -39,7 +39,7 @@ from torch.quantization import \
 
 logger = logging.getLogger(__name__)
 
-cur_qconfig = default_qconfig
+cur_qconfig = default_per_channel_qconfig #default_qconfig
 
 
 BERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
